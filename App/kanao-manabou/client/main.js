38c6f4a328590e67eccad29f6729df24ede9ca41
Sebastian von Dahlen <s-von-dahlen@web.de>
2016-06-10 13:24:51 +0200

    android and css

--- App/kanao-manabou/client/main.js
+++ App/kanao-manabou/client/main.js
@@ -2,3 +2,17 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
  
 import { name as KanaoManabou } from '/imports/ui/components/kanaoManabou/kanaoManabou';
+
+function onReady() {
+	angular.bootstrap(document, [
+		KanaoManabou
+    ], {
+		strictDi: true
+	});
+}
+ 
+if (Meteor.isCordova) {
+	angular.element(document).on('deviceready', onReady);
+} else {
+	angular.element(document).ready(onReady);
+}
